e845f62556b11cc12742433f8c23c2295e85b52e
JaffarA <jaffar.almaleki@gmail.com>
2021-12-03 16:04:09 +0000

    updated helper to use path

--- solutions/input_helper.py
+++ solutions/input_helper.py
@@ -2,5 +2,6 @@ def get_day_input_as_list(file_name, cast=int) -> list:
     """
     Reads a text file and returns a list of lines cast as the type of cast.
     """
-    with open(f"../inputs/{file_name}", "r") as f:
+    path = __file__.replace("solutions/input_helper.py", "inputs/")
+    with open(f"{path}{file_name}", "r") as f:
         return [cast(_.replace("\n", "").strip()) for _ in f.readlines()]
